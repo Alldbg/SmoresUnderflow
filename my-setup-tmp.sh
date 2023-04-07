aba90e6d1a4ced2875fdac0be4eacce1b8115cf1
ollydbg <b.afellah.2018@gmail.com>
2023-04-07 00:51:00 +0000

    Fix issues

--- my-setup-tmp.sh
+++ my-setup-tmp.sh
@@ -1,10 +1,13 @@
+
+sudo pip3 install ansible
+
 sudo apt-get update
 sudo apt-get install -y rabbitmq-server
 
 curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elastic.gpg
 echo "deb [signed-by=/usr/share/keyrings/elastic.gpg] https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 sudo apt-get update
-sudo apt install elasticsearch
+sudo apt-get install -y  elasticsearch
 
 
 ansible-playbook deployment/ansible.yml -i deployment/hosts
